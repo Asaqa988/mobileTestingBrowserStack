16ce6ed4b6455501be6bbb6ec29d92816a372d08
Asaqa988 <asaqa001@gmail.com>
2024-11-17 21:21:37 +0300

    update

--- src/mytest/MyTestCases.java
+++ src/mytest/MyTestCases.java
@@ -7,14 +7,12 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class MyTestCases {
@@ -39,11 +37,11 @@ public class MyTestCases {
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("deviceName", "Google Pixel 6"); // Use an available device
 		capabilities.setCapability("appiumVersion", "1.22.0");
-		capabilities.setCapability("app", "bs://1e70c9f0534e0575d3005dd8a8f13f559db6e615");
+		capabilities.setCapability("app", "bs://e148900c46ff86faaaff070b8b0216d44fd3d2de");
 
 	}
 
-	@Test(priority = 1,enabled = false)
+	@Test(priority = 1,enabled = true)
 	public void addTwoNumbers() throws MalformedURLException {
 
 		// Wait until the element is visible before clicking
@@ -62,10 +60,11 @@ public class MyTestCases {
 		Assert.assertEquals(actual, expected);
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2,enabled = false)
 	public void clickOnTwoRandomNumbers() throws InterruptedException, MalformedURLException {
 
-		Thread.sleep(2000);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(2));
+
 		List<WebElement> allButtons = driver.findElements(By.className("android.widget.ImageButton"));
 
 		for (WebElement button : allButtons) {
